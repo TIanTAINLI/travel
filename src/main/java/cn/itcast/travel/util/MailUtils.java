cd3730ff231c6fcc8d6b4e72976a4e2d630feb9c
Yi <286900510@qq.com>
2019-08-09 15:42:45 +0800

    BaseServlet抽取

--- src/main/java/cn/itcast/travel/util/MailUtils.java
+++ src/main/java/cn/itcast/travel/util/MailUtils.java
@@ -9,8 +9,8 @@ import java.util.Properties;
  * 发邮件工具类
  */
 public final class MailUtils {
-    private static final String USER = "18684667160@163.com"; // 发件人称号，同邮箱地址
-    private static final String PASSWORD = "P1gYiiMi"; // 如果是qq邮箱可以使户端授权码，或者登录密码
+    private static final String USER = "18XXXXXXX60@163.com"; // 发件人称号，同邮箱地址
+    private static final String PASSWORD = "PXXXXXXi"; // 如果是qq邮箱可以使户端授权码，或者登录密码
 
     /**
      *
@@ -68,7 +68,7 @@ public final class MailUtils {
     }
 
     public static void main(String[] args) throws Exception { // 做测试用
-        MailUtils.sendMail("18684667160@163.com","你好，这是一封测试邮件，无需回复。","测试邮件");
+        MailUtils.sendMail("18XXXXXXX60@163.com","你好，这是一封测试邮件，无需回复。","测试邮件");
         System.out.println("发送成功");
     }
 
